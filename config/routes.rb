67903399804f1ebf0b8622309ea3bb1904137df3
Alex Henderson <ahenderson42@academic.rrc.ca>
2023-03-18 17:52:34 -0500

    Added Index, Alphabetized and Missing Email actions and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'customers/index'
-  get 'customers/show'
+  get 'customers/alphabetized'
+  get 'customers/missing_email'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'customers#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
